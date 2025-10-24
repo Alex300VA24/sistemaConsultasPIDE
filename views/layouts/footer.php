9739d2ce2981408e5519510f0a3a5af3707a8374
gamm95 <jhonatanmm.1995@gmail.com>
2025-10-24 13:07:04 -0500

    Cambiando el consulta dni

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -1,5 +1,6 @@
 <script src="<?= BASE_URL ?>assets/js/api.js"></script>
 <script src="<?= BASE_URL ?>assets/js/dashboard.js"></script>
+<script src="<?= BASE_URL ?>assets/js/consultas/consultaDNI.js"></script>
 
 </body>
 </html>
